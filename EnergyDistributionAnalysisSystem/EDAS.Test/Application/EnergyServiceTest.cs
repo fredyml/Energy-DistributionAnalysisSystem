7c81f97d45bd8f99692d845ea18c1bdab7d7eb29
fredyml <113043655+fredyml@users.noreply.github.com>
2023-07-08 21:22:52 -0500

    Se crean test unitarios

---
+++ EnergyDistributionAnalysisSystem/EDAS.Test/Application/EnergyServiceTest.cs
@@ -0,0 +1,81 @@
+﻿using EDAS.Application.Dtos;
+using EDAS.Application.Interfaces;
+using Moq;
+
+namespace EDAS.Application.Services.Tests
+{
+    public class EnergyServiceTests
+    {
+        private readonly DateTime _startDate = new DateTime(2010, 1, 1);
+        private readonly DateTime _endDate = new DateTime(2010, 1, 3);
+
+        [Fact]
+        public async Task GetHistoricalCustomerAsync_ShouldReturnDataFromRepository()
+        {
+            
+            IEnumerable<HistoricoClienteDto> expectedData = new List<HistoricoClienteDto>
+            {
+                new HistoricoClienteDto { Fecha = _startDate, Tramo = "Tramo 1", Consumo = 100, Perdidas = 10, Costo = 50 },
+                new HistoricoClienteDto { Fecha = _startDate.AddDays(1), Tramo = "Tramo 2", Consumo = 200, Perdidas = 20, Costo = 100 }
+            };
+
+            Mock<IRepository> repositoryMock = new Mock<IRepository>();
+            repositoryMock.Setup(r => r.GetHistoricalCustomerAsync(_startDate, _endDate, "Residencial")).ReturnsAsync(expectedData);
+
+            EnergyService energyService = new EnergyService(repositoryMock.Object);
+
+            
+            IEnumerable<HistoricoClienteDto> result = await energyService.GetHistoricalCustomerAsync(_startDate, _endDate, "Residencial");
+
+            
+            Assert.Equal(expectedData, result);
+            repositoryMock.Verify(r => r.GetHistoricalCustomerAsync(_startDate, _endDate, "Residencial"), Times.Once);
+        }
+
+        [Fact]
+        public async Task GetHistoricalSegmentsAsync_ShouldReturnDataFromRepository()
+        {
+            
+            IEnumerable<HistoricoTramoDto> expectedData = new List<HistoricoTramoDto>
+            {
+                new HistoricoTramoDto { Tramo = "Tramo 1", Consumo = 100, Perdidas = 10, Costo = 50 },
+                new HistoricoTramoDto { Tramo = "Tramo 2", Consumo = 200, Perdidas = 20, Costo = 100 }
+            };
+
+            Mock<IRepository> repositoryMock = new Mock<IRepository>();
+            repositoryMock.Setup(r => r.GetHistoricalSegmentsAsync(_startDate, _endDate)).ReturnsAsync(expectedData);
+
+            EnergyService energyService = new EnergyService(repositoryMock.Object);
+
+            
+            IEnumerable<HistoricoTramoDto> result = await energyService.GetHistoricalSegmentsAsync(_startDate, _endDate);
+
+            
+            Assert.Equal(expectedData, result);
+            repositoryMock.Verify(r => r.GetHistoricalSegmentsAsync(_startDate, _endDate), Times.Once);
+        }
+
+        [Fact]
+        public async Task GetTopSegmentsCustomerAsync_ShouldReturnDataFromRepository()
+        {
+            
+            IEnumerable<TopTramosClienteDto> expectedData = new List<TopTramosClienteDto>
+            {
+                new TopTramosClienteDto { Tramo = "Tramo 1", Perdidas = 10 },
+                new TopTramosClienteDto { Tramo = "Tramo 2", Perdidas = 20 }
+            };
+
+            Mock<IRepository> repositoryMock = new Mock<IRepository>();
+            repositoryMock.Setup(r => r.GetTopSegmentsCustomerAsync(_startDate, _endDate)).ReturnsAsync(expectedData);
+
+            EnergyService energyService = new EnergyService(repositoryMock.Object);
+
+            
+            IEnumerable<TopTramosClienteDto> result = await energyService.GetTopSegmentsCustomerAsync(_startDate, _endDate);
+
+           
+            Assert.Equal(expectedData, result);
+            repositoryMock.Verify(r => r.GetTopSegmentsCustomerAsync(_startDate, _endDate), Times.Once);
+        }
+    }
+}
--- EnergyDistributionAnalysisSystem/EDAS.Test/Infrastructure/EFRepositoryTest.cs
+++ EnergyDistributionAnalysisSystem/EDAS.Test/Infrastructure/EFRepositoryTest.cs
@@ -1,10 +1,85 @@
-﻿using EDAS.Infrastructure.Persitence;
+﻿using EDAS.Domain.Models;
+using EDAS.Infrastructure.Persitence;
 using Microsoft.EntityFrameworkCore;
-using Moq;
-namespace EDAS.Test.Infrastructure
+namespace EDAS.Test
 {
     public class EFRepositoryTest
     {
-       
+        private readonly DateTime _startDate = new DateTime(2010, 1, 1);
+        private readonly DateTime _endDate = new DateTime(2010, 1, 3);
+        [Fact]
+        public void TestGetHistoricalCustomerAsync()
+        {
+           
+            using (var context = CreateContext())
+            {
+                SeedDatabase(context);
+                var repository = new EFRepository(context);
+                
+                var result = repository.GetHistoricalCustomerAsync(_startDate, _endDate, "Residencial").Result;
+                
+                Assert.NotNull(result);
+                Assert.NotEmpty(result);
+            }
+        }
+        [Fact]
+        public void TestGetHistoricalSegmentsAsync()
+        {
+            
+            using (var context = CreateContext())
+            {
+                SeedDatabase(context);
+                var repository = new EFRepository(context);
+                
+                var result = repository.GetHistoricalSegmentsAsync(_startDate, _endDate).Result;
+               
+                Assert.NotNull(result);
+                Assert.NotEmpty(result);
+            }
+        }
+        [Fact]
+        public void TestGetTopSegmentsCustomerAsync()
+        { 
+            using (var context = CreateContext())
+            {
+                SeedDatabase(context);
+                var repository = new EFRepository(context);
+                
+                var result = repository.GetTopSegmentsCustomerAsync(_startDate, _endDate).Result;
+               
+                Assert.NotNull(result);
+                Assert.NotEmpty(result);
+            }
+        }
+
+        private EnergyDistributionAnalysisSystemContext CreateContext()
+        {
+            var databaseName = Guid.NewGuid().ToString();
+            var options = new DbContextOptionsBuilder<EnergyDistributionAnalysisSystemContext>()
+                .UseInMemoryDatabase(databaseName: databaseName)
+                .Options;
+            return new EnergyDistributionAnalysisSystemContext(options);
+        }
+        private void SeedDatabase(EnergyDistributionAnalysisSystemContext context)
+        {
+            var fecha1 = new Fecha { FechaId = 1, Fecha1 = _startDate };
+            var fecha2 = new Fecha { FechaId = 2, Fecha1 = _startDate.AddDays(1) };
+            var fecha3 = new Fecha { FechaId = 3, Fecha1 = _endDate };
+            context.Fechas.AddRange(new List<Fecha> { fecha1, fecha2, fecha3 });
+            var linea1 = new Linea { LineaId = 1, Nombre = "Tramo1" };
+            context.Lineas.Add(linea1);
+            var sector1 = new Sector { SectorId = 1, Nombre = "Residencial" };
+            context.Sectors.Add(sector1);
+            context.Consumos.Add(new Consumo { Fecha = fecha1, Linea = linea1, Sector = sector1, Valor = 100 });
+            context.Consumos.Add(new Consumo { Fecha = fecha2, Linea = linea1, Sector = sector1, Valor = 200 });
+            context.Consumos.Add(new Consumo { Fecha = fecha3, Linea = linea1, Sector = sector1, Valor = 300 });
+            context.Costos.Add(new Costo { Fecha = fecha1, Linea = linea1, Sector = sector1, Valor = 50 });
+            context.Costos.Add(new Costo { Fecha = fecha2, Linea = linea1, Sector = sector1, Valor = 100 });
+            context.Costos.Add(new Costo { Fecha = fecha3, Linea = linea1, Sector = sector1, Valor = 150 });
+            context.Perdida.Add(new Perdidum { Fecha = fecha1, Linea = linea1, Sector = sector1, Valor = 10 });
+            context.Perdida.Add(new Perdidum { Fecha = fecha2, Linea = linea1, Sector = sector1, Valor = 20 });
+            context.Perdida.Add(new Perdidum { Fecha = fecha3, Linea = linea1, Sector = sector1, Valor = 30 });
+            context.SaveChanges();
+        }
     }
 }
